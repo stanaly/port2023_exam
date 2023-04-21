7aa35efa98ea2c7ce357f36d8867f54d4248ae21
stanaly <tlsgusrb70@gmail.com>
2023-04-21 16:32:37 +0900

    4. Connection의 Dependency를 Client로 그리고, Client에서 Factory로 이동하시오.(Dependency Injection)

--- src/test/java/kr/ac/jejunu/user/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/user/UserDaoTests.java
@@ -9,14 +9,15 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
+
     @Test
     public void get() throws SQLException, ClassNotFoundException {
         Long id = 1l;
         String name = "hulk";
         String password = "1234";
 
-        ConnectionMaker connectionMaker = new JejuConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.getUserDao();
 
         User user = userDao.findById(id);
 
@@ -33,48 +34,9 @@ public class UserDaoTests {
         User user = new User();
         user.setName(name);
         user.setPassword(password);
-        ConnectionMaker connectionMaker = new JejuConnectionMaker();
 
-        UserDao userDao = new UserDao(connectionMaker);
-
-        userDao.insert(user);
-
-        assertThat(user.getId(), greaterThan(1l));
-
-        User insertedUser = userDao.findById(user.getId());
-
-        assertThat(insertedUser.getId(),  is(user.getId()));
-        assertThat(insertedUser.getName(), is(name));
-        assertThat(insertedUser.getPassword(), is(password));
-    }
-
-    @Test
-    public void getForHalla() throws SQLException, ClassNotFoundException {
-        Long id = 1l;
-        String name = "hulk";
-        String password = "1111";
-
-        ConnectionMaker connectionMaker = new HallaConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-
-        User user = userDao.findById(id);
-
-        assertThat(user.getId(), is(id));
-        assertThat(user.getName(), is(name));
-        assertThat(user.getPassword(), is(password));
-    }
-
-    @Test
-    public void insertForHalla() throws SQLException, ClassNotFoundException {
-        String name = "신현규";
-        String password = "1234";
-
-        User user = new User();
-        user.setName(name);
-        user.setPassword(password);
-
-        ConnectionMaker connectionMaker = new HallaConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao userDao = daoFactory.getUserDao();
 
         userDao.insert(user);
 
